045d2106dfb7b831af2612d589eb65341e631ed4
PedroLopes7K <pedrohenri3601@gmail.com>
2022-10-19 23:23:44 -0300

    final crud app

--- cap5/crud/db.js
+++ cap5/crud/db.js
@@ -1,4 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb')
+
+global.globalString = 'This can be accessed anywhere!'
+
 async function connect() {
   if (global.db) return global.db
   const conn = await MongoClient.connect('mongodb://localhost:27017/')
@@ -12,4 +15,26 @@ async function findAll() {
   return db.collection('customers').find().toArray()
 }
 
-module.exports = { findAll }
+async function insert(customer) {
+  const db = await connect()
+  return db.collection('customers').insertOne(customer)
+}
+
+async function findOne(id) {
+  const db = await connect()
+  const objId = new ObjectId(id)
+  return db.collection('customers').findOne(objId)
+}
+
+async function update(id, customer) {
+  const filter = { _id: new ObjectId(id) }
+  const db = await connect()
+  return db.collection('customers').updateOne(filter, { $set: customer })
+}
+
+async function deleteOne(id) {
+  const db = await connect()
+  const filter = { _id: new ObjectId(id) }
+  return db.collection('customers').deleteOne(filter)
+}
+module.exports = { findAll, insert, findOne, update, deleteOne }
--- cap5/crud/routes/index.js
+++ cap5/crud/routes/index.js
@@ -4,14 +4,52 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  // console.log('to aqui msm')
+  // console.log(globalString)
   res.render('index', { docs: await db.findAll() })
 })
 
 router.get('/new', function (req, res, next) {
-  res.render('new', { title: 'Cadastro de Cliente', action: '/new' })
+  res.render('new', { title: 'Cadastro de Cliente', action: '/new', doc: {} })
 })
-router.post('/new', function (req, res, next) {
+
+// GET to delete
+router.get('/delete/:id', async function (req, res, next) {
+  const id = req.params.id
+  await db.deleteOne(id)
+  res.redirect('/?delete=true')
+})
+
+/* POST edit page. */
+router.post('/edit/:id', async function (req, res) {
+  console.log('estou chegando aqui')
+  const id = req.params.id
+  const nome = req.body.nome
+  const idade = parseInt(req.body.idade)
+  const uf = req.body.uf
+  await db.update(id, { nome, idade, uf })
+  res.redirect('/?edit=true')
+})
+
+/* POST new page. */
+router.post('/new', async function (req, res) {
+  console.log('conseguindo criar normal')
+  const nome = req.body.nome
+  const idade = parseInt(req.body.idade)
+  const uf = req.body.uf
+  await db.insert({ nome, idade, uf })
   res.redirect('/?new=true')
+})
+
+/* GET edit page. */
+router.get('/edit/:id', async function (req, res, next) {
+  const id = req.params.id
+  const doc = await db.findOne(id)
+  res.render('new', {
+    title: 'Edição de Cliente',
+    doc,
+    action: '/edit/' + doc._id
+  })
 })
 
 module.exports = router
